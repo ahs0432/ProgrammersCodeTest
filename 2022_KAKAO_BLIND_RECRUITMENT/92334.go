63a4788e85544fc1c2784b613dece7c2fca86b8e
ahs0432 <ahs0432@naver.com>
2022-05-22 11:25:03 +0900

    Added KAKAO BLIND 1Q

---
+++ 2022_KAKAO_BLIND_RECRUITMENT/92334.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	fmt.Fprintln(writer, solution([]string{"muzi", "frodo", "apeach", "neo"}, []string{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2))
+	fmt.Fprintln(writer, solution([]string{"con", "ryan"}, []string{"ryan con", "ryan con", "ryan con", "ryan con"}, 3))
+}
+
+func solution(id_list []string, report []string, k int) []int {
+	reporter := map[string][]string{}
+	reported := map[string]int{}
+	var stoped []int
+
+	for _, i := range report {
+		slice := strings.Split(i, " ")
+		if slice[0] == slice[1] {
+			continue
+		}
+
+		if reporter[slice[0]] == nil {
+			reporter[slice[0]] = ([]string{slice[1]})
+			reported[slice[1]] += 1
+		} else {
+			checked := true
+			for _, j := range reporter[slice[0]] {
+				if j == slice[1] {
+					checked = false
+					break
+				}
+			}
+
+			if checked {
+				reporter[slice[0]] = append(reporter[slice[0]], slice[1])
+				reported[slice[1]] += 1
+			}
+		}
+	}
+
+	for _, j := range id_list {
+		count := 0
+
+		for _, l := range reporter[j] {
+			if reported[l] >= k {
+				count += 1
+			}
+		}
+
+		if stoped == nil {
+			stoped = []int{count}
+		} else {
+			stoped = append(stoped, count)
+		}
+	}
+
+	return stoped
+}
