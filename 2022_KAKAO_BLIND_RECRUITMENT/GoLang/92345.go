0767755e55521351ddecf2e1a0ff7922e271ce6d
ahs0432 <ahs0432@naver.com>
2022-06-01 20:35:19 +0900

    Modified 92345 failed / Checking....

--- 2022_KAKAO_BLIND_RECRUITMENT/GoLang/92345.go
+++ 2022_KAKAO_BLIND_RECRUITMENT/GoLang/92345.go
@@ -17,131 +17,142 @@ func testing_92345() {
 	defer writer.Flush()
 
 	fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, []int{1, 0}, []int{1, 2}))
-	//fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}, []int{1, 0}, []int{1, 2}))
-	//fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 0}, {1, 0, 1}, {1, 0, 1}}, []int{1, 0}, []int{1, 2}))
-	//fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1, 1, 1}}, []int{0, 0}, []int{0, 4}))
-	//fmt.Fprintln(writer, solution_92345([][]int{{1}}, []int{0, 0}, []int{0, 0}))
+	fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}, []int{1, 0}, []int{1, 2}))
+	fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1, 1, 1}}, []int{0, 0}, []int{0, 4}))
+	fmt.Fprintln(writer, solution_92345([][]int{{1}}, []int{0, 0}, []int{0, 0}))
+	fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1, 1}, {1, 1, 1, 1}}, []int{1, 0}, []int{1, 3}))
+	fmt.Fprintln(writer, solution_92345([][]int{{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}}, []int{2, 0}, []int{2, 4}))
 }
 
 func solution_92345(board [][]int, aloc []int, bloc []int) int {
 	count := playerLocation(board, true, aloc, bloc, counter{0, 0, 0})
-	//fmt.Println(count)
 	return count.aCount + count.bCount
 }
 
-func playerLocation(board [][]int, turn bool, aloc []int, bloc []int, count counter) counter {
-	var ableLoc [][]int
-	var tmpCountList []counter
-	//fmt.Println(board, count)
+func playerLocation(board [][]int, turn bool, myloc []int, oploc []int, count counter) counter {
+	myLocTmp := make([]int, 2)
+	opLocTmp := make([]int, 2)
 
-	if turn {
-		ableLoc = locAdd(board, ableLoc, aloc)
-		if ableLoc == nil {
+	copy(myLocTmp, myloc)
+	copy(opLocTmp, oploc)
+
+	ableLoc := locAdd(board, myLocTmp)
+
+	if ableLoc == nil {
+		if turn {
 			count.winner = 2
-			return count
-		}
-
-		for _, loc := range ableLoc {
-			boardTmp := make([][]int, len(board))
-			for i := 0; i < len(board); i++ {
-				boardTmp[i] = make([]int, len(board[i]))
-				copy(boardTmp[i], board[i])
-			}
-			//copy(boardTmp, board)
-
-			boardTmp[aloc[0]][aloc[1]] = 0
-			if aloc[0] == bloc[0] && aloc[1] == bloc[1] {
-				count.aCount++
-				count.winner = 1
-				return count
-			}
-
-			count.aCount++
-			if tmpCountList == nil {
-				tmpCountList = []counter{playerLocation(boardTmp, false, loc, bloc, count)}
-			} else {
-				tmpCountList = append(tmpCountList, playerLocation(boardTmp, false, loc, bloc, count))
-			}
-			count.aCount--
-		}
-	} else {
-		ableLoc = locAdd(board, ableLoc, bloc)
-		if ableLoc == nil {
+		} else {
 			count.winner = 1
-			return count
+		}
+		return count
+	}
+
+	if myLocTmp[0] == opLocTmp[0] && myLocTmp[1] == opLocTmp[1] {
+		if turn {
+			count.aCount++
+			count.winner = 1
+		} else {
+			count.bCount++
+			count.winner = 2
+		}
+		return count
+	}
+
+	var tmpCountList []counter
+	var nowCount counter
+
+	for _, loc := range ableLoc {
+		boardTmp := make([][]int, len(board), len(board[0]))
+		for i := 0; i < len(board); i++ {
+			boardTmp[i] = make([]int, len(board[i]))
+			copy(boardTmp[i], board[i])
 		}
 
-		for _, loc := range ableLoc {
-			boardTmp := make([][]int, len(board), len(board[0]))
-			for i := 0; i < len(board); i++ {
-				boardTmp[i] = make([]int, len(board[i]))
-				copy(boardTmp[i], board[i])
-			}
-			//copy(boardTmp, board)
-			boardTmp[bloc[0]][bloc[1]] = 0
-			if bloc[0] == aloc[0] && bloc[1] == aloc[1] {
-				count.bCount++
-				count.winner = 2
+		boardTmp[myLocTmp[0]][myLocTmp[1]] = 0
+		if loc[0] == opLocTmp[0] && loc[1] == opLocTmp[1] {
+			ableOpLoc := locAdd(boardTmp, opLocTmp)
 
+			if ableOpLoc == nil {
+				if turn {
+					count.winner = 1
+					count.aCount++
+				} else {
+					count.winner = 2
+					count.bCount++
+				}
 				return count
-			}
-
-			count.bCount++
-			if tmpCountList == nil {
-				tmpCountList = []counter{playerLocation(boardTmp, true, aloc, loc, count)}
 			} else {
-				tmpCountList = append(tmpCountList, playerLocation(boardTmp, true, aloc, loc, count))
+				if len(ableLoc) != 1 {
+					continue
+				}
 			}
+		}
+
+		ableLocReCheck := locAdd(boardTmp, loc)
+		if ableLocReCheck == nil && len(ableLoc) != 1 {
+			continue
+		} else {
+			if len(ableLocReCheck) == 1 && ableLocReCheck[0][0] == opLocTmp[0] && ableLocReCheck[0][1] == opLocTmp[1] && len(ableLoc) != 1 {
+				continue
+			}
+		}
+
+		if turn {
+			count.aCount++
+		} else {
+			count.bCount++
+		}
+
+		tmpCount := playerLocation(boardTmp, !turn, oploc, loc, count)
+		boardTmp[myLocTmp[0]][myLocTmp[1]] = 1
+
+		if turn {
+			count.aCount--
+		} else {
 			count.bCount--
 		}
+
+		/*
+			if nowCount.winner == 0 && tmpCount.winner != 0 {
+				nowCount = tmpCount
+			} else {
+				if tmpCount.winner != 0 && nowCount.aCount+nowCount.bCount >= tmpCount.aCount+tmpCount.bCount {
+					nowCount = tmpCount
+				}
+
+					if tmpCount.winner == 1 && nowCount.bCount < tmpCount.bCount {
+						nowCount = tmpCount
+					} else if tmpCount.winner == 2 && nowCount.aCount < tmpCount.aCount {
+						nowCount = tmpCount
+					}
+			}
+		*/
+
+		if tmpCount.winner != 0 {
+			if tmpCountList == nil {
+				tmpCountList = []counter{tmpCount}
+			} else {
+				tmpCountList = append(tmpCountList, tmpCount)
+			}
+		}
 	}
 
-	//fmt.Println(tmpCountList)
-	var tmpCount counter
-
-	for _, countL := range tmpCountList {
-		if tmpCount.winner == 0 && countL.winner != 0 {
-			tmpCount = countL
-			continue
-		}
-
-		if countL.winner == 1 {
-			if tmpCount.winner == 1 {
-				if countL.aCount <= tmpCount.aCount {
-					if countL.bCount >= tmpCount.bCount {
-						tmpCount = countL
-					}
-				}
-			} else {
-				if countL.aCount <= tmpCount.bCount {
-					if countL.bCount >= tmpCount.aCount {
-						tmpCount = countL
-					}
-				}
-			}
+	for _, tmpCount := range tmpCountList {
+		if nowCount.winner == 0 {
+			nowCount = tmpCount
 		} else {
-			if tmpCount.winner == 1 {
-				if countL.bCount <= tmpCount.aCount {
-					if countL.aCount >= tmpCount.bCount {
-						tmpCount = countL
-					}
-				}
-			} else {
-				if countL.bCount <= tmpCount.bCount {
-					if countL.bCount >= tmpCount.aCount {
-						tmpCount = countL
-					}
-				}
+			if tmpCount.winner != 0 && nowCount.aCount+nowCount.bCount >= tmpCount.aCount+tmpCount.bCount {
+				nowCount = tmpCount
 			}
 		}
 	}
 
-	//fmt.Println(tmpCount)
-
-	return tmpCount
+	return nowCount
 }
 
-func locAdd(board [][]int, ableLoc [][]int, loc []int) [][]int {
+func locAdd(board [][]int, loc []int) [][]int {
+	var ableLoc [][]int
+
 	if loc[0] != 0 && board[loc[0]-1][loc[1]] != 0 {
 		ableLoc = locAddList(ableLoc, []int{loc[0] - 1, loc[1]})
 	}
