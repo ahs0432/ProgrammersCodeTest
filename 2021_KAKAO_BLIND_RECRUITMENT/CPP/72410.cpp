4654005a85750f64d06045eaa09d2babe7915a3f
ahs0432 <ahs0432@naver.com>
2022-06-09 00:13:29 +0900

    Modified 72410 Q

--- 2021_KAKAO_BLIND_RECRUITMENT/CPP/72410.cpp
+++ 2021_KAKAO_BLIND_RECRUITMENT/CPP/72410.cpp
@@ -19,48 +19,38 @@ string solution(string new_id) {
     new_id = regex_replace(new_id, regex("[^a-z0-9-_.]"), "");
 
     // 3 Step
-    char backChar = 0;
-    for (int i = 1; i < new_id.size(); i++) {
-        char backChar = new_id[i-1];
-        if (backChar == '.' && new_id[i] == '.' ) {
-            new_id[i-1] = 0;
+    for (int i = 0; i < new_id.size(); i++) {
+        if (!answer.empty() && answer.back() == '.' && new_id[i] == '.') {
+            continue;
         }
+        answer += new_id[i];
     }
-    new_id = regex_replace(new_id, regex("[^a-z0-9-_.]"), "");
 
     // 4 Step
-    if (new_id[0] == '.') {
-        new_id[0] = 0;
+    if (answer.front() == '.') {
+        answer.erase(answer.begin());
     }
-    if (new_id[new_id.size()-1] == '.') {
-        new_id[new_id.size()-1] = 0;
+    if (answer.back() == '.') {
+        answer.pop_back();
     }
-
-    new_id = regex_replace(new_id, regex("[^a-z0-9-_.]"), "");
 
     // 5 Step
-    if (new_id.size() == 0) {
-        new_id = "a";
+    if (answer.empty()) {
+        answer = "a";
     // 6 Step
-    } else if (new_id.size() > 15) {
-        if (new_id[14] == '.') {
-            new_id[14] = 0;
+    } else if (answer.size() > 15) {
+        answer = answer.substr(0, 15);
+        if (answer.back() == '.') {
+            answer.pop_back();
         }
-        for (int i = 15; i < new_id.size(); i++) {
-            new_id[i] = 0;
-        }
-        new_id = regex_replace(new_id, regex("[^a-z0-9-_.]"), "");
     } 
 
     // 7 Step
-    if (new_id.size() < 3) {
-        char a = new_id[new_id.size()-1];
-        for (int i = new_id.size(); i < 3; i++) {
-            new_id+=a;
+    if (answer.size() < 3) {
+        for (int i = answer.size(); i < 3; i++) {
+            answer+=answer.back();
         }
     }
-
-    answer = new_id;
 
     return answer;
 }
