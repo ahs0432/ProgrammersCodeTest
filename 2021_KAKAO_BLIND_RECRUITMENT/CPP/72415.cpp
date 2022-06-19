bf24fa62cc3d5691462224641b9a70ac1cb3ca7e
ahs0432 <ahs0432@naver.com>
2022-06-19 21:44:39 +0900

    Modified 72415 Failed / Accu 80%

--- 2021_KAKAO_BLIND_RECRUITMENT/CPP/72415.cpp
+++ 2021_KAKAO_BLIND_RECRUITMENT/CPP/72415.cpp
@@ -5,123 +5,103 @@
 
 using namespace std;
 
-int cardMove(vector<vector<int>> board, int src, int dst, int otherLoc, bool type) {
-    int result = 0;
-
-    if (type) {
-        bool dstChange = false;
-        if (dst < src) {
-            dstChange = true;
-            int tmp = dst;
-            dst = src;
-            src = tmp;
-        }
-
-        bool oneTouch = false;
-        if (dst == 3 || board[dst][otherLoc] != 0 || (dstChange && board[src][otherLoc] != 0)) {
-            oneTouch = true;
-        }
-
-        for (int i = src+1; i <= dst; i++) {
-            if (!oneTouch) {
-                result++;
-                continue;
-            }
-
-            if (board[i][otherLoc] != 0) {
-                result++;
-            }
-        }
-
-        if (result == 0) {
-            result = 1;
-        }
-    } else {
-        bool dstChange = false;
-        if (dst < src) {
-            dstChange = true;
-            int tmp = dst;
-            dst = src;
-            src = tmp;
-        }
-
-        bool oneTouch = false;
-        if (dst == 3 || board[otherLoc][dst] != 0 || (dstChange && board[otherLoc][src] != 0)) {
-            oneTouch = true;
-        }
-
-        for (int i = src+1; i <= dst; i++) {
-            if (!oneTouch) {
-                result++;
-                continue;
-            }
-
-            if (board[otherLoc][i] != 0) {
-                result++;
-                continue;
-            }
-        }
-
-        if (result == 0) {
-            result = 1;
-        }
+// 정확도 80%
+int cardMove(vector<vector<int>> board, vector<int> src, vector<int> dst, bool type, int count) {
+    if (src[0] == dst[0] && src[1] == dst[1]) {
+        return count;
+    } else if (src[type] == dst[type]) {
+        return cardMove(board, src, dst, !type, count);
     }
-
-    return result;
-}
-
-int cardMoveCalc(vector<vector<int>> board, vector<int> src, vector<int> dst) {
-    if (dst[0] == src[0] && dst[1] == src[1]) {
-        return 0;
-    }
-
-    int xStart = 0;
-    int yStart = 0;
-
-    if (dst[0] != src[0]) {
-        xStart += cardMove(board, src[0], dst[0], src[1], true);
-        if (dst[1] != src[1]) yStart += cardMove(board, src[0], dst[0], dst[1], true);
-    }
-
-    if (dst[1] != src[1]) {
-        yStart += cardMove(board, src[1], dst[1], src[0], false);
-        if (dst[0] != src[0]) xStart += cardMove(board, src[1], dst[1], dst[0], false);
-    }
-
-    if (xStart == 0 && yStart == 0) {
-        return 0;
-    } else if (xStart == 0) {
-        return yStart;
-    } else if (yStart == 0) {
-        return xStart;
-    } else if (xStart <= yStart) {
-        return xStart;
-    }
-
-    return yStart;
-}
-
-int calcCard(vector<vector<int>> board, vector<int> cardList, map<int, vector<vector<int>>> cardLocation, vector<int> nowLoc, int cardNum, int count) {
-    cardList.erase(cardList.begin() + cardNum);
 
     int lowCount = 0;
-    for (int i = 0; i < cardList.size(); i++) {
-        int move1 = calcCard(board, cardList, cardLocation, cardLocation[cardList[i]][1], i, count + cardMoveCalc(board, nowLoc, cardLocation[cardList[i]][0]) + cardMoveCalc(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1]) + 2);
-        int move2 = calcCard(board, cardList, cardLocation, cardLocation[cardList[i]][0], i, count + cardMoveCalc(board, nowLoc, cardLocation[cardList[i]][1]) + cardMoveCalc(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0]) + 2);
+    bool oneTouch = false;
+    int oneTouchTarget = 0;
+    int oneTouchCount = 0;
 
-        int tmpCount = 0;
-        if (move1 <= move2) {
-            tmpCount = move1;
-        } else {
-            tmpCount = move2;
+    for (int i = 1; i <= abs(dst[type]-src[type]); i++) {
+        int absCount = i;
+        if (dst[type] < src[type]) {
+            absCount = -absCount;
         }
 
+        if (type && board[src[!type]][src[type]+absCount] == 0) {
+            oneTouch = true;
+        } else if (!type && board[src[type]+absCount][src[!type]] == 0) {
+            oneTouch = true;
+        } else if (oneTouch && oneTouchTarget == 0 && type && (src[type]+absCount == 3 || src[type]+absCount == 0 || board[src[!type]][src[type]+absCount] != 0)) {
+            oneTouchTarget = i;
+            oneTouchCount += 1;
+            break;
+        } else if (oneTouch && oneTouchTarget == 0 && !type && (src[type]+absCount == 3 || src[type]+absCount == 0 || board[src[type]+absCount][src[!type]] != 0)) {
+            oneTouchTarget = i;
+            oneTouchCount += 1;
+            break;
+        } else {
+            oneTouchCount += 1;
+        }
+    }
+
+    for (int i = 1; i <= abs(dst[type]-src[type]); i++) {
+        int addCount = 0;
+        int moving = i;
+        if (oneTouchTarget == i) {
+            addCount = oneTouchCount;
+        } else if (oneTouch && oneTouchTarget < i) {
+            addCount = oneTouchCount + 1;
+        } else {
+            addCount = i;
+        }
+
+        if (dst[type] < src[type]) {
+            moving = -moving;
+        } 
+
+        src[type] += moving;
+        int tmpCount = cardMove(board, src, dst, !type, count+addCount);
         if (lowCount == 0 || tmpCount < lowCount) {
             lowCount = tmpCount;
         }
+        src[type] -= moving;
     }
     
-    if (lowCount == 0) return count;
+    return lowCount;
+}
+
+int compareMove(int move1, int move2) {
+    if (move1 <= move2) {
+        return move1;
+    } else {
+        return move2;
+    }
+}
+
+int calcMove(vector<vector<int>> board, vector<int> cardList, map<int, vector<vector<int>>> cardLocation, vector<int> nowLoc, int cardNum, int count) {
+    for (vector<int> loc : cardLocation[cardList[cardNum]]) {
+        board[loc[0]][loc[1]] = 0;
+    }
+    
+    cardList.erase(cardList.begin() + cardNum);
+    if (cardList.size() == 0) {
+        return count;
+    }
+
+    int lowCount = 0;
+    
+    for (int i = 0; i < cardList.size(); i++) {
+        int move = compareMove(
+            calcMove(board, cardList, cardLocation, cardLocation[cardList[i]][1], i, 
+                count + compareMove(cardMove(board, nowLoc, cardLocation[cardList[i]][0], true, 0), cardMove(board, nowLoc, cardLocation[cardList[i]][0], false, 0))
+                + compareMove(cardMove(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1], true, 2), cardMove(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1], false, 2))),
+            calcMove(board, cardList, cardLocation, cardLocation[cardList[i]][0], i, 
+                count + compareMove(cardMove(board, nowLoc, cardLocation[cardList[i]][1], true, 0), cardMove(board, nowLoc, cardLocation[cardList[i]][1], false, 0))
+                + compareMove(cardMove(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0], true, 2), cardMove(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0], false, 2))));
+
+        if (lowCount == 0 || lowCount > move) {
+            lowCount = move;
+        }
+       
+    }
+
     return lowCount;
 }
 
@@ -145,19 +125,17 @@ int solution(vector<vector<int>> board, int r, int c) {
         }
     }
 
-    for (int i = 0; i < cardList.size(); i++) {
-        int move1 = calcCard(board, cardList, cardLocation, cardLocation[cardList[i]][1], i, cardMoveCalc(board, vector<int>{r,c}, cardLocation[cardList[i]][0]) + cardMoveCalc(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1]) + 2);
-        int move2 = calcCard(board, cardList, cardLocation, cardLocation[cardList[i]][0], i, cardMoveCalc(board, vector<int>{r,c}, cardLocation[cardList[i]][1]) + cardMoveCalc(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0]) + 2);
+    for (int i = 0; i< cardList.size(); i++) {
+        int move = compareMove(
+            calcMove(board, cardList, cardLocation, cardLocation[cardList[i]][1], i, 
+                compareMove(cardMove(board, vector<int>{r, c}, cardLocation[cardList[i]][0], true, 0), cardMove(board, vector<int>{r, c}, cardLocation[cardList[i]][0], false, 0))
+                + compareMove(cardMove(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1], true, 2), cardMove(board, cardLocation[cardList[i]][0], cardLocation[cardList[i]][1], false, 2))),
+            calcMove(board, cardList, cardLocation, cardLocation[cardList[i]][0], i, 
+                compareMove(cardMove(board, vector<int>{r, c}, cardLocation[cardList[i]][1], true, 0), cardMove(board, vector<int>{r, c}, cardLocation[cardList[i]][1], false, 0))
+                + compareMove(cardMove(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0], true, 2), cardMove(board, cardLocation[cardList[i]][1], cardLocation[cardList[i]][0], false, 2))));
 
-        int tmpCount = 0;
-        if (move1 <= move2) {
-            tmpCount = move1;
-        } else {
-            tmpCount = move2;
-        }
-
-        if (answer == 0 || tmpCount < answer) {
-            answer = tmpCount;
+        if (answer == 0 || answer > move) {
+            answer = move;
         }
     }
 
@@ -167,5 +145,6 @@ int solution(vector<vector<int>> board, int r, int c) {
 int main() {
     cout << solution(vector<vector<int>>{{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}}, 1, 0) << endl;
     cout << solution(vector<vector<int>>{{3,0,0,2},{0,0,1,0},{0,1,0,0},{2,0,0,3}}, 1, 0) << endl;
+    cout << solution(vector<vector<int>>{{1,5,0,2},{6,4,3,0},{0,2,1,5},{3,0,6,4}}, 0, 0) << endl; // Answer 32;;;
     return 0;
 }
