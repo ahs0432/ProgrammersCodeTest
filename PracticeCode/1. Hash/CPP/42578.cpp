4c363847bfac2e96c749820837535e198b68f312
ahs0432 <ahs0432@naver.com>
2022-06-23 00:04:16 +0900

    Modified 42578 Q

--- PracticeCode/1. Hash/CPP/42578.cpp
+++ PracticeCode/1. Hash/CPP/42578.cpp
@@ -5,53 +5,30 @@
 
 using namespace std;
 
-// 정확성 96.4%, TC 1 Timeout
-int dfs(map<string, vector<string>> clothe, bool putOn) {
-    int total = 0;
-
-    if (clothe.size() == 1) {
-        if (putOn) {
-            total += 1;
-        }
-        total += clothe.begin()->second.size();
-    } else {
-        int count = clothe.begin()->second.size() + 1;
-        clothe.erase(clothe.begin()->first);
-        
-        for (int i = 0; i < count; i++) {
-            if (i == 0) {
-                total += dfs(clothe, putOn);
-            } else {
-                total += dfs(clothe, true);
-            }
-        }
-    }
-
-    return total;
-}
-
+// 정확도 100%
 int solution(vector<vector<string>> clothes) {
-    map<string, vector<string>> clothe;
-
+    map<string, int> clothe;
     for (vector<string> c : clothes) {
-        if (clothe[c[1]].empty()) {
-            clothe[c[1]] = vector<string>{c[0]};
+        clothe[c[1]] += 1;
+    }
+
+    int answer = 0;
+
+    for (map<string, int>::iterator iter = clothe.begin(); iter!=clothe.end(); iter++) {
+        if (answer == 0) {
+            answer = (iter->second + 1);
         } else {
-            clothe[c[1]].push_back(c[0]);
+            answer *= (iter->second + 1);
         }
     }
 
-    if (clothe.size() == 1) {
-        return clothes.size();
-    }
-
-    int answer = dfs(clothe, false);
-    
+    answer -= 1;
     return answer;
 }
 
 int main() {
     cout << solution(vector<vector<string>>{{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}}) << endl;
     cout << solution(vector<vector<string>>{{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}}) << endl;
+    
     return 0;
 }
