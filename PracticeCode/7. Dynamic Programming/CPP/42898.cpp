3f2dc27661bba75dfe155fe6255883f07cd024fc
ahs0432 <ahs0432@naver.com>
2022-07-15 00:01:23 +0900

    Added 42898 Q

--- PracticeCode/7. Dynamic Programming/CPP/42898.cpp
+++ PracticeCode/7. Dynamic Programming/CPP/42898.cpp
@@ -4,37 +4,43 @@
 
 using namespace std;
 
-// 정확성 테스트 90% / TC 8 Timeout
-int moveLoc(vector<vector<int>> nowMap, vector<int> nowLoc) {
-    if (nowMap[nowLoc[1]][nowLoc[0]] == 1) {
-        return 1;
-    }
-
-    int result = 0;
-    if (nowLoc[1]+1 != nowMap.size() && nowMap[nowLoc[1]+1][nowLoc[0]] != -1) {
-        result += moveLoc(nowMap, vector<int>{nowLoc[0], nowLoc[1]+1});
-    }
-    
-    if (nowLoc[0]+1 != nowMap[0].size() && nowMap[nowLoc[1]][nowLoc[0]+1] != -1) {
-        result += moveLoc(nowMap, vector<int>{nowLoc[0]+1, nowLoc[1]});
-    }
-
-    return result % 1000000007;
-}
-
 int solution(int m, int n, vector<vector<int>> puddles) {
     vector<vector<int>> nowMap(n, vector<int>(m, 0));
     
     for (vector<int> puddle : puddles) {
         nowMap[puddle[1]-1][puddle[0]-1] = -1;
     }
-    nowMap[n-1][m-1] = 1;
+    nowMap[0][0] = 1;
 
-    return moveLoc(nowMap, vector<int>{0,0});
+    int answer = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            if (nowMap[i][j] != -1) {
+                if (i >= 1 && nowMap[i-1][j] != -1) {
+                    nowMap[i][j] += nowMap[i-1][j];
+                }
+                if (j >= 1 && nowMap[i][j-1] != -1) {
+                    nowMap[i][j] += nowMap[i][j-1];
+                }
+            }
+
+            if (nowMap[i][j] >= 1000000007) {
+                nowMap[i][j] %= 1000000007;
+            }
+        }
+    }
+
+    return nowMap[n-1][m-1];
 }
 
 int main() {
     cout << solution(4, 3, vector<vector<int>>{{2, 2}}) << endl;
+    cout << solution(2, 2, vector<vector<int>>{{1,0},{0,1}}) << endl;
 
     return 0;
 }
