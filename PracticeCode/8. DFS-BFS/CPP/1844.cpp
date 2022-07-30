e6c6271281c451adba662b588da847a654fbe9cd
ahs0432 <ahs0432@naver.com>
2022-07-31 00:02:56 +0900

    Update 1844 Q / vector 시간... 덕분에 Queue

--- PracticeCode/8. DFS-BFS/CPP/1844.cpp
+++ PracticeCode/8. DFS-BFS/CPP/1844.cpp
@@ -4,35 +4,33 @@
 
 using namespace std;
 
-// 정확도 100%, 효율성 50%
+// 정확도 100%, 효율성 100%
 int solution(vector<vector<int>> maps) {
     int xmax = (maps.size() - 1);
     int ymax = (maps[0].size() - 1);
 
-    vector<vector<int>> moveLoc = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    vector<pair<int, int>> moveLoc = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     
     // nowloc, count
-    queue<pair<vector<int>, int>> q;
+    queue<pair<pair<int, int>, int>> q;
     q.push({{0, 0}, 1});
     maps[0][0] = 0;
 
     while(!q.empty()) {
-        if (q.front().first[0] == xmax && q.front().first[1] == ymax) {
+        if (q.front().first.first == xmax && q.front().first.second == ymax) {
             return q.front().second;
         }
 
-        int nextCount = q.front().second + 1;
+        for (pair<int, int> m : moveLoc) {
+            int nextX = q.front().first.first + m.first;
+            int nextY = q.front().first.second + m.second;
 
-        for (vector<int> m : moveLoc) {
-            int nextX = q.front().first[0] + m[0];
-            int nextY = q.front().first[1] + m[1];
-
-            if (nextX < 0 || nextY < 0 || nextX > xmax || nextY > ymax || maps[nextX][nextY] == 0) {
+            if ((nextX < 0 || nextY < 0 || nextX > xmax || nextY > ymax) || maps[nextX][nextY] == 0 ) {
                 continue;
             }
 
             maps[nextX][nextY] = 0;
-            q.push({{nextX, nextY}, nextCount});
+            q.push({{nextX, nextY}, q.front().second + 1});
         }
 
         q.pop();
