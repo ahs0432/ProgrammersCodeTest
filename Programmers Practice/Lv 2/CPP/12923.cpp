773152c261a8eaa9489f49bba04bc0d867c8c6e8
ahs0432 <ahs0432@naver.com>
2022-09-13 00:16:04 +0900

    Update 12923 Q / 겨우.. 풀었다..

--- Programmers Practice/Lv 2/CPP/12923.cpp
+++ Programmers Practice/Lv 2/CPP/12923.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// 효율성 실패 3 - 이전까지는 시간 초과
+// 지문 상 10,000,000번 까지 블록을 놓는다는 내용을 참고해야함...
 vector<int> solution(long long begin, long long end) {
     vector<int> answer;
 
@@ -15,7 +15,7 @@ vector<int> solution(long long begin, long long end) {
         
         bool check = false;
         for (int j = 2; j * j <= i; j++) {
-            if (i % j == 0) {
+            if (i % j == 0 && i / j <= 10000000) {
                 answer.push_back(i / j);
                 check = true;
                 break;
