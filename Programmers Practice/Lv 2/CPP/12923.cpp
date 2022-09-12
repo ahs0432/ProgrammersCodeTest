10c1a84b0ddb0c3bcf8ee77da6e930950706a455
ahs0432 <ahs0432@naver.com>
2022-09-12 21:37:05 +0900

    Added 12923 Failed..

--- Programmers Practice/Lv 2/CPP/12923.cpp
+++ Programmers Practice/Lv 2/CPP/12923.cpp
@@ -3,37 +3,27 @@
 
 using namespace std;
 
-// 효율성 실패 2
+// 효율성 실패 3
 vector<int> solution(long long begin, long long end) {
     vector<int> answer;
-    vector<bool> isPrime(end + 1, true);
-    
-    isPrime[0] = false;
-    isPrime[1] = false;
-    
-    for(long long i = 2; i * i <= end; i++){
-        if(isPrime[i]){
-            for(long long j = i * i; j <= end; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
-    
+
     for (int i = begin; i <= end; i++) {
         if (i == 1) {
             answer.push_back(0);
             continue;
         }
         
-        if (isPrime[i]) {
-            answer.push_back(1);
-        } else {
-            for (long long j = 2; j <= i/2; j++) {
-                if (i % j == 0) {
-                    answer.push_back(i / j);
-                    break;
-                }
+        bool check = false;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                answer.push_back(i / j);
+                check = true;
+                break;
             }
+        }
+        
+        if (!check) {
+            answer.push_back(1);
         }
     }
     
